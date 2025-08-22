9b9287452e93c9aa3cced7a0f11c5d044f429182
Mughira Rehman <mughirarehman2573@gmail.com>
2025-08-22 09:44:35 +0500

    updated paths

--- Backend/database/models.py
+++ Backend/database/models.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from sqlalchemy import String, Integer, Boolean, ForeignKey, Text, DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from db import Base
+from database.db import Base
 
 class User(Base):
     __tablename__ = "users"
