b430f945b8cd818528bef43e7d5edb470268eaa9
Mughira Rehman <mughirarehman2573@gmail.com>
2025-08-22 08:56:32 +0500

    Latest code

--- Backend/app/core/config.py
+++ Backend/app/core/config.py
@@ -10,7 +10,6 @@ REDDIT_CLIENT_SECRET = os.getenv("REDDIT_CLIENT_SECRET", "Dx0u0TIJowrE8rxiOPkoRE
 REDDIT_REDIRECT_URI = os.getenv("REDDIT_REDIRECT_URI", "http://localhost:8000/index.html")
 REDDIT_OAUTH_URL = "https://www.reddit.com/api/v1/authorize"
 REDDIT_TOKEN_URL = "https://www.reddit.com/api/v1/access_token"
-OPEN_API_KEY = "sk-proj-A5sN3-6IDONW2Df_Dy0AmGc0MdLildIi-FavfjCV1z_miRjNMvYF3tsV8af2YZttvuOBPfnOg2T3BlbkFJlGNnaSZotiC-rxAlXmZej769fmgNmCLaM0Des8o9g53CkisqCkr2KXn4pi5COSkrryrzJHfU8A2"
 
 def access_token_expires() -> timedelta:
     return timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
