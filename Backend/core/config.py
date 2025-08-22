4380dbf5c82758011d1b607324e1827ac4ed6b42
Mughira Rehman <mughirarehman2573@gmail.com>
2025-08-22 22:39:03 +0500

    config update

--- Backend/core/config.py
+++ Backend/core/config.py
@@ -1,5 +1,8 @@
 import os
 from datetime import timedelta
+from dotenv import load_dotenv
+
+load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY", "change-me-in-prod")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "10080"))
